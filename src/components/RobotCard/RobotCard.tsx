27c68ed1f61dfa45e4cfaaa44e2337306dcd17d1
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-13 22:59:55 +0200

    Add RobotCardStyled

--- src/components/RobotCard/RobotCard.tsx
+++ src/components/RobotCard/RobotCard.tsx
@@ -1,21 +1,23 @@
 import { RobotStructure } from "../../types";
+import RobotCardStyled from "./RobotCardStyled";
 
 interface RobotCardProps {
   robot: RobotStructure;
 }
 
 const RobotCard = ({
-  robot: { name, speed, endurance, dateOfCreation },
+  robot: { name, speed, endurance, dateOfCreation, imageUrl },
 }: RobotCardProps): JSX.Element => {
   return (
-    <>
-      <span>{dateOfCreation}</span>
-      <h2>{name}</h2>
-      <div>
+    <RobotCardStyled>
+      <span className="robot-card__birth">{dateOfCreation}</span>
+      <img className="robot-card__image" src={imageUrl} alt={name} />
+      <h2 className="robot-card__title">{name}</h2>
+      <div className="robot-card__points">
         <span>{speed}</span>
         <span>{endurance}</span>
       </div>
-    </>
+    </RobotCardStyled>
   );
 };
 
---
+++ src/components/RobotCard/RobotCardStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const RobotCardStyled = styled.article`
+  display: flex;
+  position: relative;
+  justify-content: center;
+  width: fit-content;
+`;
+
+export default RobotCardStyled;
