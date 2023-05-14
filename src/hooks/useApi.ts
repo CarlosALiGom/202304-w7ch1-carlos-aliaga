6bacff25b8af3ec471018e3c23b61690f0a32ed2
Frank Pereira <frankpereirag000@gmail.com>
2023-05-14 22:11:08 +0200

    Type getRobotd function

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 import { useCallback } from "react";
+import { RobotState } from "../store/robots/robotsSlice";
 
 export const apiUrl = import.meta.env.VITE_APP_API_URL;
 
 const useApi = () => {
-  const getRobots = useCallback(async () => {
-    const {
-      data: { robots },
-    } = await axios.get(`${apiUrl}/robots`);
+  const getRobots = useCallback(async (): Promise<RobotState> => {
+    const { data: robots } = await axios.get<RobotState>(`${apiUrl}/robots`);
     return robots;
   }, []);
 
