9f2ecc2b7c2bdf6f7aa5067dab9f7b459888d59b
Frank Pereira <frankpereirag000@gmail.com>
2023-05-14 11:57:13 +0200

    Create useApi custom hook

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { useCallback } from "react";
+
+export const apiUrl = import.meta.env.VITE_APP_API_URL;
+
+const useApi = () => {
+  const getRobots = useCallback(async () => {
+    const { data: robots } = await axios.get(`${apiUrl}robots`);
+    return robots;
+  }, []);
+
+  return { getRobots };
+};
+
+export default useApi;
